77de4d870a865499f1150efd1c5b85c3051e7686
Nate Ebel <n8ebel@gmail.com>
2020-03-01 16:36:44 -0800

    Add structure and error checking to location search screen

--- app/src/main/java/com/goobar/io/premiseweather/search/SearchModule.kt
+++ app/src/main/java/com/goobar/io/premiseweather/search/SearchModule.kt
@@ -1,5 +1,6 @@
 package com.goobar.io.premiseweather.search
 
+import com.goobar.io.premiseweather.search.ui.SearchViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
